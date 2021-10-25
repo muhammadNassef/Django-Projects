f54c8c0fe70e03c40bdf515d5e4538e156ed672c
Muhammad Nassef <38579751+muhammadNassef@users.noreply.github.com>
2021-10-25 06:39:52 +0200

    Update views.py

--- Entries/views.py
+++ Entries/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import DetailView, ListView
+from django.urls import reverse_lazy
+from django.views.generic import DetailView, ListView, CreateView, UpdateView, DeleteView
 from .models import Entry
 
 
@@ -13,4 +14,24 @@ class EntryListView(ListView):
 class EntryDetailView(DetailView):
     model = Entry
     context_object_name = 'entry_detail'
+
+class EntryCreateView(CreateView):
+    model = Entry
+    fields = ['title', 'content', 'high_priority']
+    success_url = reverse_lazy('Diary:entry-list')
+    context_object_name = 'entry_create'
+
+
+class EntryUpdateView(UpdateView):
+    model = Entry
+    fields = ['title', 'content', 'high_priority']
+    context_object_name = 'entry_update'
+
+    def get_success_url(self):
+        return reverse_lazy("Diary:entry-detail", {"pk": self.entry.id})
+
+
+class EntryDeleteView(DeleteView):
+    model = Entry
+    success_url = reverse_lazy('Diary:entry-list')
 
